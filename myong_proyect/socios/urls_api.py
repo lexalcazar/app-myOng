591d8ddaa9a9c3a39147dbba493cc1adfe2b3de0
Alexander Alcázar <aalcazarprogramacion@gmail.com>
2026-03-11 21:31:08 +0100

    Ejercicio 1 completo y ejercicio 2 a medias

--- myong_proyect/socios/urls_api.py
+++ myong_proyect/socios/urls_api.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api_views import SocioViewSet, PagoViewSet, pagos_por_socio
+from .api_views import SocioViewSet, PagoViewSet, endpoint_check_dni, pagos_por_socio
 
 # Crea el router y registra los ViewSets
 router = DefaultRouter()
-router.register(r'socios', SocioViewSet)      # Genera /api/socios/
+router.register(r'socios', SocioViewSet, basename='socio')      # Genera /api/socios/
 router.register(r'pagos', PagoViewSet, basename='pago')  # Genera /api/pagos/
 
 # Las URLs generadas incluyen:
@@ -12,7 +12,10 @@ router.register(r'pagos', PagoViewSet, basename='pago')  # Genera /api/pagos/
 # /socios/{id}/     -> retrieve (GET), update (PUT/PATCH), destroy (DELETE)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    
     # Endpoint personalizado para pagos por socio
     path('socios/<uuid:socio_id>/pagos/', pagos_por_socio, name='api_pagos_socio'),
+    # Endpoint funcional adicional para validar DNI
+    path('socios/check-dni/', endpoint_check_dni, name='api_check_dni'),
+    path('', include(router.urls)),
 ]
