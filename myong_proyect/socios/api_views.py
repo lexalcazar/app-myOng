6cf5f6d716da26dc1730aa6ef6ba6c08990e1766
Dario Ureña <darioaxel@gmail.com>
2026-03-11 14:25:57 +0100

    feat: jwt

--- myong_proyect/socios/api_views.py
+++ myong_proyect/socios/api_views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from datetime import date
 
